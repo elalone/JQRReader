ac3cd256a6ebe4f4853611dbbe39cf5fb95be835
elalone <72115905+elalone@users.noreply.github.com>
2025-09-19 07:35:23 -0400

    Create JSONtoCSV.py

---
+++ JSONtoCSV.py
@@ -0,0 +1,19 @@
+import json
+from csv import DictWriter
+
+with open('PCTEExport.json', 'rb') as f:
+    data = json.load(f)
+
+rows = []
+for item in data['data']:
+    rows.append({
+        'Name': item['competencyName'],
+        'Description': item['competencyDesc'],
+        'Status': item['status'],
+    })
+
+with open('signoffs.csv', 'w', newline='') as f:
+    fieldnames = ['Name', 'Description', 'Status']
+    writer = DictWriter(f, fieldnames=fieldnames)
+    writer.writeheader()
+    writer.writerows(rows)
